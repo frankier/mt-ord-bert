79f05607795a228c5b25cfde80ba3ed042f5855f
Frankie Robertson <frankie@robertson.name>
2022-12-08 08:36:05 +0200

    Make prep_rt generic

--- prep_rt.py
+++ prep_rt.py
@@ -1,16 +1,27 @@
-import sys
 from bert_ordinal.datasets import load_data, save_to_disk_with_labels
 from mt_ord_bert_utils import tokenize
+import argparse
 
 
-dataset, num_labels, _ = load_data(
-    "multiscale_rt_critics", num_dataset_proc=8,
-)
-dataset = dataset.map(
-    tokenize,
-    input_columns="text",
-    batched=True,
-    desc="Tokenizing",
-    num_proc=8,
-)
-save_to_disk_with_labels(sys.argv[1], dataset, num_labels)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dataset")
+    parser.add_argument("out")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    dataset, num_labels, _ = load_data(args.dataset, num_dataset_proc=8)
+    dataset = dataset.map(
+        tokenize,
+        input_columns="text",
+        batched=True,
+        desc="Tokenizing",
+        num_proc=8,
+    )
+    save_to_disk_with_labels(args.out, dataset, num_labels)
+
+
+if __name__ == "__main__":
+    main()
